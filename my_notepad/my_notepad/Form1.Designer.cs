320230d87e6581bb5b3d9e347976d8e5c18d0f5d
Tima Saetov <timasaetov@gmail.com>
2023-09-11 20:52:29 +0500

    red form and delete button

--- my_notepad/my_notepad/Form1.Designer.cs
+++ my_notepad/my_notepad/Form1.Designer.cs
@@ -28,27 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.button1.Location = new System.Drawing.Point(258, 162);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 63);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "закрыть";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BackColor = System.Drawing.Color.Red;
             this.ClientSize = new System.Drawing.Size(728, 450);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "блокнот";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -57,8 +44,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
